f18fa358dadc3990064e3a9597946fe4322720f2
Po-Hsuan Kuo <0104neil0104@gmail.com>
2022-11-24 20:49:15 +0800

    我做的事情
    123456789

--- headers/good.h
+++ headers/good.h
@@ -1,1 +1,16 @@
-int good;
+#include<iostream>
+using namespace std;
+class Vector{
+    int n;
+    double* m;
+    Vector();
+}
+Vector::Vector
+
+
+int main(){
+    int good;
+    good = 47797968;
+    return 0;
+}
+
